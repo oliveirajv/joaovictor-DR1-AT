73921b2d96a474fa04c87262537196949d8eddc9
luffy <johnwinolive@outlook.com>
2022-12-19 04:40:28 -0200

    Alterações/adições feitas pelo professor Criação dos novos métodos getDir(), getSubDir(), temFilho(), getPai() e getCompleteName(). Opção "cd" criada

--- directory.py
+++ directory.py
@@ -3,12 +3,30 @@ from random import randrange
 from colored import fg, attr
 
 class Directory:
-    def __init__(self, _dir_name):
+    def __init__(self, _dir_name, _dir_pai=None):
         self._dir_name = _dir_name
         # Conteúdo do diretório
         self._dir_files = {}
         self._sub_dir = {}
-        self._parent = None
+        self._parent = _dir_pai
+    # Pega diretório
+    def get_directory(self, _dir_name):
+        dir = self._sub_dir[_dir_name]
+        return dir
+
+    def temFilho(self, subdir):
+        if (subdir != '..'):
+            if (subdir in self._sub_dir):
+                return True
+            return False
+        return True
+    
+    def getSubDir(self, subdir):
+        if (subdir != '..'):
+            if (self.temFilho(subdir)):
+                return self._sub_dir.get(subdir)
+            return None
+        return self._dir_pai
     # Cria arquivo
     def create_file(self, _file_name):
         _file_size = randrange(1, 8096)
@@ -21,12 +39,13 @@ class Directory:
         color = fg("slate_blue_1")
         reset = attr("reset")
         for items in self._dir_files:
-            print(" " + items , self._dir_files[items])
+            print(" " , items , self._dir_files[items])
         for items in self._sub_dir:
             print(color , items , self._sub_dir[items] , reset)
     # Cria sub diretório
     def make_sub_dir(self, _sub_dir):
-        self._sub_dir[_sub_dir._dir_name] = _sub_dir._dir_files ,  _sub_dir._sub_dir
+        dir = Directory(_sub_dir, self)
+        self._sub_dir[dir._dir_name] = dir
     # Deleta sub diretório
     def delete_sub_dir(self, _dir_name):
         del self._sub_dir[_dir_name]    
@@ -35,12 +54,30 @@ class Directory:
         pass
     # Move arquivo
     def move_file(self, _file_name, _dir_name):
-        pass
-    # Pega diretório
-    def get_directory(self, _dir_name):
-        dir = self._sub_dir[_dir_name]
-        return dir
-    # Mostrando o objeto Directory formatado
+        file_size = self._dir_files.get(_file_name)
+        _dir_name._dir_files[_file_name] = file_size 
+        del self._dir_files[_file_name]   
+    # Mostrando a classe Directory formatado
     def __str__(self):
-        return self._dir_name + str(self._dir_files) + str(self._sub_dir)
-ext4 = Directory("/")
+        return str(self._dir_name) + str(self._dir_files) + str(self._sub_dir)
+    
+    def getPai(self):
+        return self._parent
+    
+    def getCompleteName(self):
+        complete_path = [self._dir_name]
+        pai = self.getPai()
+
+        while (pai != None):            
+            complete_path.insert(0, pai._dir_name)
+            pai = pai.getPai()        
+        result = ''
+        if len(complete_path) > 1:
+            for i,dir in enumerate(complete_path):
+                if (i == 0):
+                    result += dir
+                else:
+                    result += dir + '/'
+        else:
+            result = '/'
+        return result
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-""" folder = input("joao@joao:~$ ")
-print(f"joao@joao:~/{folder}$") """
-from directory import Directory, ext4
+from directory import Directory
+
+def getDir(caminho, caminho_relativo):
+    # Este método busca se há o caminho de diretórios 
+    # formado pelos elementos da lista "caminho" passada como parâmetro.
+    # O parâmetro booleano "caminho_relativo" especifica se os
+    # elementos da lista devem ser buscados em caminhos
+    # relativos ao diretório atual (dir1/...) ou a partir da raiz (/dir1/...)
+    # Caso o caminho não seja encontrado, a função retorna None
+    # Caso contrário, é retornado o objeto Diretorio do final do caminho
+        if (caminho_relativo):
+            prox_dir = atual
+        else:
+            prox_dir = raiz        
+        ## Enquanto for possível descer na árvore, pega o próximo
+        ## diretório do caminho especificado
+        for d in caminho:
+            if (prox_dir.temFilho(d)):
+                prox_dir = prox_dir.getSubDir(d)
+            else:
+                return None            
+        return prox_dir
+
+raiz = Directory("/")
+atual = raiz
+
 # Tupla com as opções disponíveis para o usuário
-options = ("new", "del", "ls", "mkdir", "rmdir", "cp", "mv", "help", "exit", "test")
+options = ("new", "del", "ls", "mkdir", "rmdir", "cp", "mv", "help", "exit", "test", "cd")
 # Fazendo um while que termina quando o usuário escolha a opção "exit"
 while True:
     # Criando um input para o usuário escolher as opções
-    user_input = input("joao@joao:~$ ")
+    user_input = input("joao@joao:" + atual.getCompleteName() + "$ ")
     command = user_input.split(" ")[0]
     # Fazendo um while para dizer que a opção não existe, enquanto o usuário não escreve uma opção dentre as existentes
     while command not in options:
@@ -16,78 +39,94 @@ while True:
         # Cria um arquivo
     if command == "new":
         file_name = user_input.split(" ")[1]
-        ext4.create_file(file_name)
+        raiz.create_file(file_name)
     # Opção "del"
         # Exlcui um arquivo
     if command == "del":
         remove_file_name = user_input.split(" ")[1]
-        if file_name not in ext4._dir_files:
+        if file_name not in raiz._dir_files:
             print("File does not exist")
         else:
-            del ext4._dir_files[remove_file_name]
+            del raiz._dir_files[remove_file_name]
     # Opção "ls"
         # Mostra todo o conteúdo dos diretórios
-    if command == "ls":
-        print("/")
-        ext4.list_all()
+    if command == "ls":        
+        atual.list_all()
     # Opção "mkdir"
         # Cria um novo diretório
     if command == "mkdir":
-        user_option = user_input.split(" ")
-        if len(user_option) == 3:
-            new_dir_name = user_option[1]
-            new_dir_name = Directory(new_dir_name)
-            dir_name = user_option[2]
-            dir = ext4.get_directory(dir_name)
-            dir._sub_dir[new_dir_name._dir_name] = new_dir_name._sub_dir
-            print(dir._sub_dir)
-            print(new_dir_name)
+        user_option = user_input.split(" ")[1]
+        caminho = user_option.split('/')
+        
+        novo_dir = caminho.pop()
+
+        if user_option[0] == '/':
+            caminho_relativo = False
+            ## Removendo o primeiro elemento vazio da lista
+            caminho.pop(0)
+            local = getDir(caminho, caminho_relativo)
         else:
-            new_dir = user_option[1]
-            # Criando um novo diretório, fora do diretório raiz (que é "/")
-            new_dir = Directory(new_dir)
-            # Colocando o novo diretório dentro do raiz
-            ext4.make_sub_dir(new_dir)
+            caminho_relativo = True
+            if len(caminho) == 0:      
+                local = atual
+            else:
+                local = getDir(caminho, caminho_relativo)        
+        local.make_sub_dir(novo_dir)            
     # Opção "rmdir"
         # Remove o diretório selecionado
     if command == "rmdir":
         remove_dir_name = user_input.split(" ")[1]
-        if remove_dir_name not in ext4._sub_dir:
+        if remove_dir_name not in raiz._sub_dir:
             print("Dir does not exist")
         else:
-            ext4.delete_directory(remove_dir_name)
+            raiz.delete_sub_dir(remove_dir_name)
     # Opção "cp"
         # Faz uma cópia do arquivo para um novo diretório
     if command == "cp":
         file_name = user_input.split(" ")[1]
         dir_name = user_input.split(" ")[2]
-        if file_name not in ext4._dir_files:
+        if file_name not in raiz._dir_files:
             print("File does not exist")
-        elif dir_name not in ext4._sub_dir:
+        elif dir_name not in raiz._sub_dir:
             print("Dir does not exist")
         else:
-            dir = ext4.get_directory(dir_name)
-            copy_file_value = ext4._dir_files.get(file_name)
+            dir = raiz.get_directory(dir_name)
+            copy_file_value = raiz._dir_files.get(file_name)
             dir._dir_files[file_name] = copy_file_value
     # Opção "mv"
         # Move o arquivo para um novo diretório e exclui do anterior
     if  command == "mv":
         file_name = user_input.split(" ")[1]
         dir_name = user_input.split(" ")[2]
-        if file_name not in ext4._dir_files:
+        if file_name not in raiz._dir_files:
             print("File does not exist") 
-        elif dir_name not in ext4._sub_dir:
+        elif dir_name not in raiz._sub_dir:
             print("Dir does not exist")        
         else:
-            copy_file_value = ext4._dir_files.get(file_name)
-            dir = ext4.get_directory(dir_name)
+            copy_file_value = raiz._dir_files.get(file_name)
+            dir = raiz.get_directory(dir_name)
             dir._dir_files[file_name] = copy_file_value            
-            del ext4._dir_files[file_name]
+            del raiz._dir_files[file_name]
+    if command == "cd":
+        user_option = user_input.split(" ")[1]
+        caminho = user_option.split('/')          
+
+        if user_option[0] == '/':
+            caminho_relativo = False
+            ## Removendo o primeiro elemento vazio da lista
+            caminho.pop(0)
+            atual = getDir(caminho, caminho_relativo)
+        else:
+            caminho_relativo = True
+            if len(caminho) == 0:      
+                pass
+            else:
+                atual = getDir(caminho, caminho_relativo)
     # Opção "help"
         # Mostra os comandos disponíveis
     if command == "help":
         for option in options:
-            print(" " + option)
+            print(" " , option)
     # Opção "test"
     if command == "test":
         pass
